4cf87a3844e22cd11fa3c4a7a155a2e6603fad62
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-09-07 14:17:44 -0600

    added tables and more menu navigation, altered sql structure slightly to handle auto-increment

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,25 @@ CREATE DATABASE minionTracker_db;
 USE minionTracker_db;
 
 CREATE TABLE departments (
-    id INT,
+    id INT AUTO_INCREMENT,
     d_name VARCHAR(100),
     d_desc VARCHAR(100),
     PRIMARY KEY (d_name)
 );
 
 CREATE TABLE roles(
-    id INT,
+    id INT AUTO_INCREMENT,
     title VARCHAR(100),
     r_desc VARCHAR(100),
     PRIMARY KEY (title)
 );
 
 CREATE TABLE employees (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   roleTitle VARCHAR(100),
   deptName VARCHAR(100),
+  managerID INT,
   PRIMARY KEY (id),
   CONSTRAINT FOREIGN KEY (roleTitle) REFERENCES roles(title),
   CONSTRAINT FOREIGN KEY (deptName) REFERENCES departments(d_name)
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Employee = require("./lib/Employee");
 const Department = require("./lib/Department");
 const Role = require("./lib/Role");
 const Question = require("./lib/Question");
-const db = mysql.createConnection(
+var db = mysql.createConnection(
     {
       host: 'localhost',
       // MySQL username,
@@ -16,8 +16,11 @@ const db = mysql.createConnection(
       database: 'minionTracker_db'
     },
     console.log(`Connected to the minionTracker_db database.`)
-  );
+);
 
+async function main(){
+    displayMainMenu();
+}
 
 const qTypes = {
     input: "input",
@@ -34,38 +37,38 @@ const qTypes = {
 function displayMainMenu(){
     let q = new Question(qTypes.list,"\n","Options",["View All Departments","View All Roles","View All Employees","Add a Department","Add a Role","Add an Employee","Update Employee Role"]);
     inquirer.prompt([q]).then((response)=>{
+        let testRole=new Role(2,"minion","should unionize");
+        let testDept = new Department(1,"Mailroom","The room for mail");
         switch (response.Options) {
             case "View All Departments":
-                db.query('SELECT * FROM departments', function (err, results) {
-                    console.log(results);
-                  });
+                getAvailable("departments");
                 break;
             case "View All Roles":
-                db.query('SELECT * FROM roles', function (err, results) {
-                    console.log(results);
-                  });
+                getAvailable("roles");
                 break;
             case "View All Employees":
-                db.query('SELECT * FROM employees', function (err, results) {
-                    console.log(results);
-                  });
+                getAvailable("employees");
                 break;
             case "Add a Department":
-                let testDept = new Department(1,"Mailroom","The room for mail");
-                db.query('INSERT INTO departments (id,d_name,d_desc) VALUES ?',[[testDept.getArray()]],function (err, results) {
-                    if (err) throw err;  
-                    console.log(results);
-                  });
+                
+                // db.query('INSERT INTO departments (id,d_name,d_desc) VALUES ?',[[testDept.getArray()]],function (err, results) {
+                //     if (err) throw err;  
+                //     console.log(results);
+                //   });
                 break;
             case "Add a Role":
-                let testRole=new Role(1,"boss","He's the boss");
-                db.query('INSERT INTO roles (id,title,r_desc) VALUES ?',[[testRole.getArray()]],function (err, results) {
-                    if (err) throw err;  
-                    console.log(results);
-                  });
+                
+                // db.query('INSERT INTO roles (id,title,r_desc) VALUES ?',[[testRole.getArray()]],function (err, results) {
+                //     if (err) throw err;  
+                //     console.log(results);
+                //   });
                 break;
             case "Add an Employee":
-               
+
+                // db.query('INSERT INTO employees (id,name,roleTitle,deptName,managerID) VALUES ?',[[testEmployee.getArray()]],function (err, results) {
+                //     if (err) throw err;  
+                //     console.log(results);
+                //   });
                 break;
             case "Update Employee Role":
             
@@ -76,8 +79,41 @@ function displayMainMenu(){
     });
 }
 
+function returnToMainMenu(){
+    let q = new Question(qTypes.confirm,"Return To Main Menu?","navConfirm");
+    inquirer.prompt([q]).then((response)=>{
+        if(response.navConfirm){
+            displayMainMenu();
+        }else{
+            returnToMainMenu();
+        }
+    });
+
+}
+
+function getAvailable(table){
+    db.query('SELECT * FROM '+table, function (err, results) {
+        console.table(results);
+        returnToMainMenu();
+      });
+}
+
+function promptNewRole(){
+    let q = [new Question(qTypes.input,"What is the new role's title?","roleTitle"),new Question(qTypes.input,"What is the role's description?","rollDesc")];
+    inquirer.prompt(q).then((response)=>{
+        if(response.roleTitle){
+            let r = new Role()
+            displayMainMenu();
+        }else{
+            returnToMainMenu();
+        }
+    });
+}
+
+
+
 function view(table){
 
 }
 
-displayMainMenu();
+main();
